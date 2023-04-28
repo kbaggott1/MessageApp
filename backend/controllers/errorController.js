780099b1e4fcdc9acaf24c23516511745acdf510
Cristiano Fazi <cristianofazi03@gmail.com>
2023-04-28 13:29:10 -0400

    Added .env file
    added .env file and error controller

---
+++ backend/controllers/errorController.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const routeRoot = '*';
+
+router.get('*', showErrorPage);
+async function showErrorPage(request, response){
+    response.status(404);
+    response.send('Invalid URL entered. Please try again.')
+}
+
+module.exports = {
+    router,
+    routeRoot,
+    showErrorPage
+}
