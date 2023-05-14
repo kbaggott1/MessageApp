5d5ea01c3297b4fae11ed2cb3dc7a8a6d86ea133
sebg-dev <44349956+sebg-dev@users.noreply.github.com>
2023-05-14 15:27:44 -0400

    added a few unit tests

--- backend/tests/chatControllerTest.test.js
+++ backend/tests/chatControllerTest.test.js
@@ -64,3 +64,187 @@ afterEach(async () => {
     await mongod.stop();
     console.log("Mongo mock connection stopped");
 });
+
+test("POST /chats success case", async () => {
+    const user1 = generateValidUserData();
+    const user2 = generateValidUserData();
+
+    const user1Response = await testRequest.post("/users/").send(user1);
+
+    const user2Response = await testRequest.post("/users/").send(user2);
+
+    const user1Id = user1Response.body.id;
+    const user2Id = user2Response.body.id;
+
+    const chatResponse = await testRequest.post("/chats/").send({
+        userSenderId: user1Id,
+        userRecipientId: user2Id
+    });
+
+    const cursor = await model.getCollection().find();
+    const results = await cursor.toArray();
+
+    expect(chatResponse.status).toBe(200);
+    expect(Array.isArray(results)).toBe(true);
+    expect(results.length).toBe(1);
+    expect(results[0].userSenderId).toBe(user1Id);
+    expect(results[0].userRecipientId).toBe(user2Id);
+});
+
+test("POST /chats failure invalid userSenderId failure case", async () => {
+    const user1 = generateValidUserData();
+    const user2 = generateValidUserData();
+
+    const user1Response = await testRequest.post("/users/").send(user1);
+
+    const user2Response = await testRequest.post("/users/").send(user2);
+
+    const user1Id = user1Response.body.id;
+    const user2Id = user2Response.body.id;
+
+    const chatResponse = await testRequest.post("/chats/").send({
+        userSenderId: null,
+        userRecipientId: user2Id
+    });
+
+    const cursor = await model.getCollection().find();
+    const results = await cursor.toArray();
+
+    expect(chatResponse.status).toBe(400);
+    expect(Array.isArray(results)).toBe(true);
+    expect(results.length).toBe(0);
+});
+
+test("POST /chats failure database error failure case", async () => {
+    const user1 = generateValidUserData();
+    const user2 = generateValidUserData();
+
+    const user1Response = await testRequest.post("/users/").send(user1);
+
+    const user2Response = await testRequest.post("/users/").send(user2);
+
+    const user1Id = user1Response.body.id;
+    const user2Id = user2Response.body.id;
+
+    await mongod.stop();
+
+    const chatResponse = await testRequest.post("/chats/").send({
+        userSenderId: user1Id,
+        userRecipientId: user2Id
+    });
+
+    expect(chatResponse.status).toBe(500);
+
+    mongod = await MongoMemoryServer.create();
+    const url = await mongod.getUri();
+    await model.initialize("Test_Message_App", url, true);
+});
+
+test("GET /chats/:id success case", async () => {
+    
+    const user1 = generateValidUserData();
+    const user2 = generateValidUserData();
+
+    const user1Response = await testRequest.post("/users/").send(user1);
+
+    const user2Response = await testRequest.post("/users/").send(user2);
+
+    const user1Id = user1Response.body.id;
+    const user2Id = user2Response.body.id;
+
+    const addedChat = await model.addChat(user1Id, user2Id);
+
+    const testResponse = await testRequest.get(`/chats/${addedChat._id}`);
+
+    expect(testResponse.status).toBe(200);
+    expect(testResponse.body._id).toBe(addedChat._id.toString());
+    expect(testResponse.body.userSenderId).toBe(user1Id);
+    expect(testResponse.body.userRecipientId).toBe(user2Id);
+    
+});
+
+test("GET /chats/:id failure chat does not exist", async () => {
+    const testResponse = await testRequest.get("/chats/87132568");
+
+    expect(testResponse.status).toBe(400);
+});
+
+test("GET /chats/:id failure database error", async () => {
+    const senderId = generateValidUserId();
+    const recipientId = generateValidUserId();
+    addedChat = await model.addChat(senderId, recipientId);
+    await mongod.stop();
+
+    const testResponse = await testRequest.get(`/chats/${addedChat._id}`);
+    expect(testResponse.status).toBe(500);
+
+    mongod = await MongoMemoryServer.create();
+    const url = await mongod.getUri();
+    await model.initialize("Test_Message_App", url, true);
+});
+
+test('GET /chats success case', async () => {
+    const chatArray = [];
+    chatArray[0] = { userSenderId: generateValidUserId(), userRecipientId: generateValidUserId() };
+    chatArray[1] = { userSenderId: generateValidUserId(), userRecipientId: generateValidUserId() };
+    chatArray[2] = { userSenderId: generateValidUserId(), userRecipientId: generateValidUserId() };
+    chatArray[3] = { userSenderId: generateValidUserId(), userRecipientId: generateValidUserId() };
+    
+    await model.addChat(chatArray[0].userSenderId, chatArray[0].userRecipientId);
+    await model.addChat(chatArray[1].userSenderId, chatArray[1].userRecipientId);
+    await model.addChat(chatArray[2].userSenderId, chatArray[2].userRecipientId);
+    await model.addChat(chatArray[3].userSenderId, chatArray[3].userRecipientId);
+
+    const testResponse = await testRequest.get("/chats/");
+    expect(testResponse.status).toBe(200);
+
+    for(let i = 0; i < chatArray.length; i++){
+        expect(chatArray[i].userSenderId == testResponse.body[i].userSenderId).toBe(true)
+        expect(chatArray[i].userRecipientId == testResponse.body[i].userRecipientId).toBe(true)
+    }
+
+    expect(testResponse.body.length == chatArray.length).toBe(true)
+})
+
+test('GET /chats failure no chats in the database', async () => {
+    const testResponse = await testRequest.get("/chats/");
+    expect(testResponse.status).toBe(400);
+});
+
+test('GET /chats failure database error', async () => {
+    const sender1Id = generateValidUserId();
+    const recipient1Id = generateValidUserId();
+    const sender2Id = generateValidUserId();
+    const recipient2Id = generateValidUserId();
+    
+    await model.addChat(sender1Id, recipient1Id);
+    await model.addChat(sender2Id, recipient2Id);
+
+    await mongod.stop();
+
+    const testResponse = await testRequest.get("/chats/");
+    expect(testResponse.status).toBe(500);
+
+    mongod = await MongoMemoryServer.create();
+    const url = await mongod.getUri();
+    await model.initialize("Test_Message_App", url, true);
+});
+
+test('DELETE /chats success case', async () => {
+    const senderId = generateValidUserId();
+    const recipientId = generateValidUserId();
+    const addedChat = await model.addChat(senderId, recipientId);
+
+    const testResponse = await testRequest.delete("/chats/").send({
+        chatId: addedChat._id
+    });
+
+    const cursor = await model.getCollection().find();
+    const results = await cursor.toArray();
+
+    expect(testResponse.status).toBe(200);
+    expect(Array.isArray(results)).toBe(true);
+    expect(results.length).toBe(0);
+});
+
+
