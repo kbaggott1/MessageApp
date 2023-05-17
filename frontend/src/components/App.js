b984a10c0107f85c34e8572a6390e0b578a166fc
kbaggott1 <kevin.baggott1@gmail.com>
2023-05-17 12:18:52 -0400

    editing layout for messages

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Route, Routes, Navigate } from "react-router-dom";
 import { MainLayout } from './layouts/MainLayout.js'; 
+import {MessagesLayout} from './layouts/MessagesLayout'
 import { Home } from '../pages/Home';
 import { Messages } from '../pages/Messages';
 import { Settings } from '../pages/Settings';
@@ -11,7 +12,7 @@ function App() {
     <Routes>
       <Route path="/" element={ <MainLayout/>}>
         <Route index element={ <Home /> } />
-        <Route path="/messages" element={ <Messages/>} /> {/*use outlet*/}
+        <Route path="/messages" element={ <MessagesLayout/>} />
         <Route path="/settings" element={ <Settings/> } />
         <Route path="*" element={<Navigate to="/" />} />
       </Route>
