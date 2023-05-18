fb085c13128a0494500097b42ba2df88c42649c4
sebg-dev <44349956+sebg-dev@users.noreply.github.com>
2023-05-17 23:06:06 -0400

    Started home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,5 +3,12 @@
  * @returns A JSX component containing the home page
  */
 export function Home() {
-    return <h1>Home</h1>
+    return (
+        <>
+        <h1>Welcome to our Messaging Service!</h1>
+        <hr style={{ color: "#2E4F4F", backgroundColor: "#00BFFF", height: .5, borderColor : "#2C3333" }}/>
+        <h1>Let us tell you a bit about ourselves!</h1>
+        </>
+    );
+    
 }
