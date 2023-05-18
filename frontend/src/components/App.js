d5d8cfaea56b0ce6c6be2b76a80aa94148fedb67
Cristiano <cristianofazi03@gmail.com>
2023-05-17 21:03:37 -0400

    pushin p
    pushin p

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -12,41 +12,39 @@ import { Register } from '../pages/Register';
 const LoggedInContext = createContext({
   isLoggedin: false,
   setIsLoggedIn: () => {},
+})
 
+const LoggedInUserContext = createContext({
   userdata: null,
   setUserdata: () => {}
 })
-
-/*
-const LoggedInUser = createContext({
-  userdata: null,
-  setUserdata: () => {}
-})
-*/
 
 function App() {
   const [isLoggedin, setIsLoggedIn] = useState(false);
   const [userData, setUserData] = useState(null);
 
-  const loggedInValueAndSetter = [isLoggedin, setIsLoggedIn, userData, setUserData];
+  const loggedInValueAndSetter = [isLoggedin, setIsLoggedIn];
+  const userDataGetterAndSetter = [userData, setUserData]
 
   return (
     <div className='App'>
-    <LoggedInContext.Provider value={loggedInValueAndSetter}>
-      <Routes>
-        <Route path="/" element={ <MainLayout/>}>
-          <Route index element={ <Home /> } />
-          <Route path="/login" element={ <LoginPage/> } />
-          <Route path="/messages" element={ <MessagesLayout/>} />
-          <Route path="/settings" element={ <Settings/> } />
-          <Route path="/register" element={<Register />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Route>
-      </Routes>
-    </LoggedInContext.Provider>
+        <LoggedInUserContext.Provider value={userDataGetterAndSetter}>
+      <LoggedInContext.Provider value={loggedInValueAndSetter}>
+    <Routes>
+    <Route path="/" element={ <MainLayout/>}>
+  <Route index element={ <Home /> } />
+  <Route path="/login" element={ <LoginPage/> } />
+  <Route path="/messages" element={ <MessagesLayout/>} />
+  <Route path="/settings" element={ <Settings/> } />
+  <Route path="/register" element={<Register />} />
+  <Route path="*" element={<Navigate to="/" />} />
+    </Route>
+    </Routes>
+      </LoggedInContext.Provider>
+        </LoggedInUserContext.Provider>
     </div>
   );
 }
 
 export default App;
-export {LoggedInContext}
+export {LoggedInContext, LoggedInUserContext}
