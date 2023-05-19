c1c4792e0f530c06cb0f084f37d92f018276e031
kbaggott1 <kevin.baggott1@gmail.com>
2023-05-18 22:01:59 -0400

    fixed sizing of main page

--- frontend/src/pages/AboutImages.js
+++ frontend/src/pages/AboutImages.js
@@ -1,4 +1,4 @@
-import './AboutImages.css'
+import './AboutUs.css'
 
 /**
  * Images of the 3 founders. 
@@ -8,9 +8,9 @@ export function AboutImages() {
     return (
         <>
         <div className='images'>
-        <img className="Kevin" src={process.env.PUBLIC_URL + '/Kevin.jpg'} alt="Kevin" />
-        <img className="Cristiano"src={process.env.PUBLIC_URL + '/Cristiano.jpg'} alt="Cristiano" />
-        <img className="Sebastian"src={process.env.PUBLIC_URL + '/Sebastian.png'} alt="Sebastian" />
+            <img className="Kevin" src={process.env.PUBLIC_URL + '/Kevin.jpg'} alt="Kevin" />
+            <img className="Cristiano"src={process.env.PUBLIC_URL + '/Cristiano.jpg'} alt="Cristiano" />
+            <img className="Sebastian"src={process.env.PUBLIC_URL + '/Sebastian.png'} alt="Sebastian" />
         </div>
         </>
     )
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,7 @@
 import { AboutUs } from './AboutUs';
 import { AboutImages } from './AboutImages';
+import './AboutUs.css';
 
-import { useState } from 'react';
 
 
 /**
@@ -11,8 +11,28 @@ import { useState } from 'react';
 export function Home() {
     return (
         <>
-        <AboutUs/>
-        <AboutImages/>
+        <div>
+            <h1>Welcome to our Messaging Service!</h1>
+
+            <hr style={{
+                color: "#2E4F4F", 
+                backgroundColor: "#00BFFF", 
+                height: 1, 
+                borderColor: "#2C3333",
+                width: "95%",
+                marginLeft: "auto",
+                marginRight: "auto"
+            }}/>
+
+            <h1>Let us tell you a bit about ourselves!</h1>
+            <div className='homeContainer'>
+                <AboutUs/>
+                <h1>Our founders:</h1>
+                <AboutImages />
+            </div>
+        </div>
+
+
         </>
     )
 }
